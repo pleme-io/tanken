aa166109545044911fdc67dfef4314268eb3e1a2
drzzln <drzzln@protonmail.com>
2026-03-07 19:14:19 -0500

    chore: use substrate/rust-tool-release-flake.nix + add Cargo.nix

--- flake.nix
+++ flake.nix
@@ -3,74 +3,31 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    crate2nix.url = "github:nix-community/crate2nix";
+    flake-utils.url = "github:numtide/flake-utils";
     substrate = {
       url = "github:pleme-io/substrate";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      substrate,
-      ...
-    }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-
-      mkDate =
-        longDate:
-        (nixpkgs.lib.concatStringsSep "-" [
-          (builtins.substring 0 4 longDate)
-          (builtins.substring 4 2 longDate)
-          (builtins.substring 6 2 longDate)
-        ]);
-
-      props = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-      version =
-        props.package.version
-        + "+date="
-        + (mkDate (self.lastModifiedDate or "19700101"))
-        + "_"
-        + (self.shortRev or "dirty");
-
-      pname = "tanken";
-
-      package = pkgs.rustPlatform.buildRustPackage {
-        inherit pname version;
-        src = pkgs.lib.cleanSource ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-        doCheck = false;
-        meta = {
-          mainProgram = pname;
-        };
-      };
-    in
-    {
-      packages.${system} = {
-        tanken = package;
-        default = package;
-      };
-
-      overlays.default = final: prev: {
-        tanken = self.packages.${final.system}.default;
-      };
-
+  outputs = {
+    self,
+    nixpkgs,
+    crate2nix,
+    flake-utils,
+    substrate,
+  }:
+    (import "${substrate}/lib/rust-tool-release-flake.nix" {
+      inherit nixpkgs crate2nix flake-utils;
+    }) {
+      toolName = "tanken";
+      src = self;
+      repo = "pleme-io/tanken";
+    }
+    // {
       homeManagerModules.default = import ./module {
         hmHelpers = import "${substrate}/lib/hm-service-helpers.nix" { lib = nixpkgs.lib; };
       };
-
-      devShells.${system}.default = pkgs.mkShellNoCC {
-        packages = [
-          package
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.rust-analyzer
-        ];
-      };
-
-      formatter.${system} = pkgs.nixfmt-tree;
     };
 }
